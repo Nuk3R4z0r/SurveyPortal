2923512a46a37674765c775141700e2e1942c971
Nuk3R4z0r <madsmadsen34@hotmail.com>
2014-06-03 09:42:20 +0200

    Added sql stub

---
+++ App_Code/SQLHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for SQLHandler
+/// </summary>
+public class SQLHandler
+{
+    SqlConnection conn;
+
+	public SQLHandler()
+	{
+        conn = new SqlConnection("");
+	}
+
+    public int Login(string username, string password)
+    {
+        return 0;
+    }
+}
