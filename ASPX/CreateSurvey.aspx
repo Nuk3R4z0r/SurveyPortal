a2abcaf5660995ca148dcdda51d68af82c104531
Nuk3R4z0r <madsmadsen34@hotmail.com>
2014-06-13 09:43:28 +0200

    Starting CreateSurvey implementation
    Support for creating and deleting sections

---
+++ ASPX/CreateSurvey.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CreateSurvey.aspx.cs" Inherits="ASPX_CreateSurvey" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <link href="../Content/bootstrap.min.css" rel="stylesheet" />
+    <style>
+        html,body{ height:98%;}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server" style="width:100%; height:100%">
+        <div style="float:left; width:350px; height:inherit; position:fixed" class="panel panel-default">
+            <div class="col-md-12">
+                <div class="panel-body">
+                    <div class="form-group">
+                        <asp:TextBox runat="server" class="form-control" id="propBox" placeholder="Enter property"/>
+                    </div>
+                    <div class="form-group">
+                        <asp:Button runat="server" OnClick="btnSect_Click" class="btn btn-default" ID="btnSect" Text="New Section"/>
+                    </div>
+                    <div class="form-group">
+                        <asp:Button runat="server" OnClick="btnRadio_Click" class="btn btn-default" ID="btnRadio" Text="New Radiobutton"/>
+                    </div>
+                    <div class="form-group">
+                        <asp:Button runat="server" OnClick="btnBox_Click" class="btn btn-default" ID="btnBox" Text="New Textbox"/>
+                    </div>
+                    <div class="form-group">
+                        <asp:Button runat="server" OnClick="btnDelete_Click" class="btn btn-default" ID="btnSave" Text="Save & Exit"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style="float:left; width:74%; " class="col-md-offset-3">
+            <asp:PlaceHolder ID="plcDiv" runat="server">
+            </asp:PlaceHolder>
+        </div>
+    </form>
+
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ ASPX/CreateSurvey.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+public partial class ASPX_CreateSurvey : System.Web.UI.Page
+{
+    int counter = 0;
+    List<HtmlGenericControl> panelList;
+    List<Button> btnList;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string load = Request.QueryString["load"];
+
+        panelList = new List<HtmlGenericControl>();
+        btnList = new List<Button>();
+
+        if(load != null)
+        if (HttpContext.Current.Session["panelList"] != null)
+        {
+            if (HttpContext.Current.Session["btnList"] != null)
+            {
+                btnList = (List<Button>)HttpContext.Current.Session["btnList"];
+
+                    foreach (Button btn in btnList)
+                    {
+                        btn.Click += new EventHandler(btnDelete_Click);
+                    }
+            }
+
+            panelList = (List<HtmlGenericControl>)HttpContext.Current.Session["panelList"];
+
+            foreach (HtmlGenericControl div in panelList)
+            {
+                if(btnList != null)
+                div.Controls.Add(btnList[counter]);
+
+                div.ID = "Section " + (counter + 1);
+                plcDiv.Controls.Add(div);
+
+                counter++;
+            }
+        }
+    }
+
+    protected void btnSect_Click(object sender, EventArgs e)
+    {
+        AddSection(propBox.Text);
+        SaveSession();
+    }
+
+    void SaveSession()
+    {
+        HttpContext.Current.Session["panelList"] = panelList;
+        HttpContext.Current.Session["btnList"] = btnList;
+        Response.Redirect(Request.RawUrl);
+    }
+
+    void AddSection(string title)
+    {
+        string section = "Section " + (counter + 1);
+
+        HtmlGenericControl divPanel = new HtmlGenericControl("DIV");
+        divPanel.Attributes["class"] = "panel panel-default";
+        divPanel.Attributes["runat"] = "server";
+        HtmlGenericControl divBody = new HtmlGenericControl("DIV");
+        divBody.Attributes["class"] = "panel-body";
+        HtmlGenericControl divBody2 = new HtmlGenericControl("DIV");
+        divBody2.Attributes["class"] = "panel-body";
+        divBody2.InnerHtml = "<h2>" + title + "</h2>";
+
+        Button btn = new Button();
+        btn.Text = "Delete";
+        btn.ID = counter.ToString();
+        /*btn.Attributes["OnCLick"] = "btnDelete_Click";
+        btn.Click += new EventHandler(btnDelete_Click);*/
+
+        plcDiv.Controls.Add(divPanel);
+        btnList.Add(btn);
+        divPanel.Controls.Add(divBody);
+        divBody.Controls.Add(divBody2);
+
+        propBox.Text = "";
+
+        panelList.Add(divPanel);
+
+        counter++;
+    }
+
+    protected void btnRadio_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnBox_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        int id = Convert.ToInt32(((Button)sender).ID);
+        
+        btnList.RemoveAt(id);
+
+        for (int i = 0; i < btnList.Count; i++ )
+        {
+            if(i >= id)
+            {
+                int temp = Convert.ToInt32(btnList[i].ID) - 1;
+                btnList[i].ID = temp.ToString();
+            }
+        }
+
+        panelList.RemoveAt(id);
+        SaveSession();
+    }
+}
